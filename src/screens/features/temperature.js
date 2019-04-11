7df54028ac55b0e114c5fe5350988447264d751c
luisjo81 <louisjo81@gmail.com>
2019-04-11 01:04:52 -0600

    Finalizada la screen de Registro
    - GUI finalizado.
    - Se logró conectar con el servidor y hacer POST.
    - Se hacen verificaciones a las entradas.

---
+++ src/screens/features/temperature.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react'
+import { 
+    Text,
+    View,
+    ImageBackground,
+    StyleSheet
+ } from 'react-native'
+import SwitchButton from '../../components/switch.js'
+import colors from '../../constants/colors';
+import BackgroundBlue from '../../assets/backgrounds/background-2.png';
+
+export default class Filtration extends Component {
+    static navigationOptions = {
+        title: 'Filtración',
+        headerStyle: {
+            backgroundColor: colors.black
+          },
+        headerTitleStyle: {
+            fontWeight: 'bold',
+            color: colors.white
+        },
+      };
+
+   constructor() {
+      super();
+      this.state = {
+         switchValue: true,
+      }
+   }
+
+   _toggleSwitch = (value) => {
+      this.setState({switchValue: value})
+      console.log('Switch Value: ' + value)
+   }
+
+   render() {
+      return (
+        <ImageBackground style={styles.background} source={BackgroundBlue}>
+                 <View style={styles.container}>
+                    <Text style={styles.text}>Encender/Apagar</Text>
+                    <SwitchButton
+                        _toggleSwitch = {this._toggleSwitch}
+                        switchValue = {this.state.switchValue}
+                        style={styles.switch}
+                    />
+                </View>
+        </ImageBackground>
+        );
+   }
+}
+
+const styles = StyleSheet.create({
+    background: {
+        flex: 1,
+        width: '100%',
+        height: '100%'
+    },
+    container: {
+        marginTop: '25%',
+        marginLeft: '17%',
+        width: '65%',
+        height: '30%',
+        borderWidth: 2,
+        borderColor: colors.blue2,
+        backgroundColor: 'rgba(0,0,0, .5)',
+        alignItems: 'center'
+    },
+    text: { 
+        fontSize: 15,
+        flex: 1,
+        color: colors.white,
+        fontWeight:'bold',
+        textAlign: 'center',
+        marginTop: 20,
+        //marginBottom: 20
+    },
+    switch: {
+        flex: 1        
+    }
+});     
--- src/screens/signin/index.js
+++ src/screens/signin/index.js
@@ -11,6 +11,9 @@ import {
 import colors from '../../constants/colors';
 import BackgroundBlue from '../../assets/backgrounds/background-2.png';
 
+global.ipAddress = '192.168.43.175'
+global.port = 4000
+
 export default class Signin extends Component {
     constructor(props) {
         super(props);
@@ -21,48 +24,120 @@ export default class Signin extends Component {
             email: '',
             phoneNumber: '',
             password: ''
-          };
+        };
     }
-
-    _clickEventListener() {
-        this.props.navigation.navigate('Home');
-    }
-   
+  
     _onEmailTextChanged = event => {
         this.setState({
             email: event.nativeEvent.text,
         });
-    };
+    }
 
     _onFirstNameChanged = event => {
         this.setState({
             firstName: event.nativeEvent.text,
         });
-    };
+    }
 
     _onLastNameChanged = event => {
         this.setState({
             lastName: event.nativeEvent.text,
         });
-    };
+    }
 
     _onPasswordTextChanged = event => {
         this.setState({
             password: event.nativeEvent.text,
         });
-    };
+    }
 
     _onPhoneNumberTextChanged = event => {
         this.setState({
             phoneNumber: event.nativeEvent.text,
         });
-    };
+    }
 
     _onUserNameTextChanged = event => {
         this.setState({
             userName: event.nativeEvent.text,
         });
-    };
+    }
+
+    _printUserData (){
+        console.log('Username:' + this.state.userName);
+        console.log('First Name:' + this.state.firstName);
+        console.log('Last Name:' + this.state.lastName);
+        console.log('Email:' + this.state.email);
+        console.log('Phone Number:' + this.state.phoneNumber);
+        console.log('Password:' + this.state.password);
+    }
+
+    _registerUser() {
+        url = "http://" + ipAddress + ":" + port + "/api/v1/login";
+        const formData = new FormData();
+        formData.append('username', this.state.userName),
+        formData.append('name', this.state.firstName),
+        formData.append('lastName', this.state.lastName),
+        formData.append('password', this.state.password),
+        formData.append('email', this.state.email),
+        formData.append('cell', this.state.phoneNumber)
+
+        console.log("url:"+url);
+      
+        fetch(url,{
+              method: 'POST',
+              body: formData
+              }).then(function (response) {
+                return response.json();
+              }).then(function (result) { 
+                console.log(result);
+                if(!result.error){
+                 this.setState({ 
+                                status: result.error,
+                                wholeResult: result
+                              });
+                 alert("Registro completado");
+                 console.log("User signed succesfully");
+                 this._printUserData();
+                 this._setDefaultData();
+                 this.props.navigation.navigate('Home');
+                }else{
+                    Alert.alert("Problema al registrarse, inténtelo de nuevo");
+                    console.log(result);
+                    }
+                }).catch(function (error) {
+                    console.log("ERROR: " + error);
+                    alert("Result:" + error)
+                });
+    }
+
+    _setDefaultData() {
+        this.setState({
+            userName: '',
+            firstName: '',
+            lastName: '',
+            email: '',
+            phoneNumber: '',
+            password: ''
+        });
+    }
+
+    _verifyUserForm() {
+        if (
+            this.state.userName === '' ||
+            this.state.firstName === '' ||
+            this.state.lastName === '' ||
+            this.state.email === '' ||
+            this.state.phoneNumber === '' ||
+            this.state.password === '' 
+            )
+            {
+                alert("Se deben completar todos los espacios");
+            } else {
+                this._registerUser()
+            }
+        
+    }
 
     render() {
         return (
@@ -76,33 +151,49 @@ export default class Signin extends Component {
                                 placeholderTextColor = 'rgba(255,255,255,0.7)'
                                 autoCapitalize="none"
                                 autoCorrect={false}
+                                value={this.state.userName}
+                                onChange={this._onUserNameTextChanged}
                                 style={styles.input}/>
                             <TextInput 
                                 placeholder="Nombre" 
                                 placeholderTextColor = 'rgba(255,255,255,0.7)'
                                 autoCorrect={false}
+                                value={this.state.firstName}
+                                onChange={this._onFirstNameChanged}
                                 style={styles.input}/>
                             <TextInput 
                                 placeholder="Apellido" 
                                 placeholderTextColor = 'rgba(255,255,255,0.7)'
                                 autoCorrect={false}
+                                value={this.state.lastName}
+                                onChange={this._onLastNameChanged}
                                 style={styles.input}/>
                             <TextInput 
                                 placeholder="Correo electrónico" 
                                 placeholderTextColor = 'rgba(255,255,255,0.7)'
                                 autoCapitalize="none"
                                 autoCorrect={false}
+                                value={this.state.email}
+                                onChange={this._onEmailTextChanged}
                                 style={styles.input}/>
                             <TextInput 
                                 placeholder="Número Telefónico" 
                                 placeholderTextColor = 'rgba(255,255,255,0.7)'
+                                keyboardType='phone-pad'
+                                returnKeyType='done'
+                                value={this.state.phoneNumber}
+                                onChange={this._onPhoneNumberTextChanged}
                                 style={styles.input}/>
                             <TextInput 
                                 placeholder="Contraseña"
                                 placeholderTextColor = 'rgba(255,255,255,0.7)'
-                                secureTextEntry 
+                                autoCapitalize="none"
+                                autoCorrect={false}
+                                secureTextEntry
+                                value={this.state.password}
+                                onChange={this._onPasswordTextChanged} 
                                 style={styles.input}/>
-                            <TouchableOpacity style={styles.buttonContainer} onPress={() => {this._clickEventListener()}}>
+                            <TouchableOpacity style={styles.buttonContainer} onPress={() => {this._verifyUserForm()}}>
                                 <Text style={styles.buttonText}>Ingresar</Text>
                             </TouchableOpacity>
                         </View>
